287775dc8f1d33647da6d18afc7539e5a03be114
Rodrigo Monteiro <rodrigoma@gmail.com>
2017-01-14 15:12:41 -0200

    temporizador para cada item que aparecer

--- public/validating.js
+++ public/validating.js
@@ -1,27 +1,29 @@
 
 $(document).ready(function () {
     var socket = io();
-    
- //    socket.on('hidden', function(validate) {
- //        $('#' + validate.id ).remove();
- //    });
- //    socket.on('validates', function(validate) {
-	//     $(".validates").append("<li class='validate' id='" + validate.id + "'>" +
- //                "<span class='title'>" + validate.id + "</span></li>");
-	// });
-
-	// socket.on('error', function(validate) {
-	//     $(".validates").append("<li class='error' id='" + validate.id + "'>" +
- //                "<span class='title'>" + validate.id + "</span></li>");
-	// });
 
 	socket.on('error', function(validate) {
-	    $(".validates").prepend("<li class='error' id='" + validate.id + "'>" +
-                "<span class='title'>" + validate.id + "</span></li>");
+		$(".validates").prepend("<li class='error' id='" + validate.id + "'>" +
+                "<span class='title'>" + validate.id + "</span> "+
+                "<span class='timer' id='timer" + validate.id + "'></span>"+
+                "</li>");
+	    
+		$('#' + validate.id).each(function () {
+			var timer = $(this).children("#timer" + validate.id);
+		    
+			var startTime = Date.now();
+			var interval  = setInterval(function() {
+				var elapsedTime = Date.now() - startTime;
+				timer.html((elapsedTime / 1000).toFixed(1));
+			}, 100);
+
+			$(this).children("#timer" + validate.id).attr( "interval", interval);
+		});
 	});
 
 	socket.on('ok', function(validate) {
-		$('#' + validate.id ).removeClass('error').addClass('ok');
+		$('#' + validate.id).removeClass('error').addClass('ok');
+		var interval = $('#' + validate.id).children("#timer" + validate.id).attr('interval');
+		clearInterval(interval);
 	});
-
 });
